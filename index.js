13c0be004c80a8d1f12a15fa5433d1319715bffd
Roberto Cortez <radcortez@yahoo.com>
2020-02-27 23:19:11 +0000

    Initial version for Milestone Status Github Action.

---
+++ index.js
@@ -0,0 +1,35 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const octokit = new github.GitHub(core.getInput('github.token'));
+
+    const milestoneNumber = core.getInput('milestone-number');
+    console.log(`Checking ${milestoneNumber}`);
+
+    octokit.issues.getMilestone({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        milestone_number: milestoneNumber
+    }).then(({data}) => {
+
+        console.log(`Found Milestone ${data.title}`);
+
+        if (data.open_issues > 0) {
+            core.setFailed(`Milestone ${milestoneNumber} still has ${data.open_issues} open issues!`);
+        }
+
+        console.log(`Milestone has no issues open.`)
+
+    }).catch((error) => {
+        if (error.status === 404) {
+            core.setFailed(`Milestone ${milestoneNumber} Not Found!`);
+        } else {
+            console.debug(error);
+            core.setFailed('Unknown Error!')
+        }
+    })
+
+} catch (error) {
+    core.setFailed(error.message);
+}
