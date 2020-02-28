3138710687d0f35ef6b94ad01ec0897a5779c8f6
Roberto Cortez <radcortez@yahoo.com>
2020-02-28 17:25:45 +0000

    Changed search to use Milestone title instead.

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const milestoneNumber = core.getInput('milestone-number');
-    console.log(`Checking Milestone with number ${milestoneNumber}`);
+    const milestoneTitle = core.getInput('milestone-title');
+    console.log(`Checking Milestone ${milestoneTitle}`);
 
     const octokit = new github.GitHub(core.getInput('github-token'));
 
-    octokit.issues.getMilestone({
+    octokit.issues.listMilestonesForRepo({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
-        milestone_number: milestoneNumber
+        state: 'open',
     }).then(({data}) => {
 
-        console.log(`Found Milestone ${data.title}`);
+        let milestone = data.find(function (milestone) {
+            return milestone.title === milestoneTitle;
+        });
 
-        if (data.open_issues > 0) {
-            core.setFailed(`Milestone ${data.title} still has ${data.open_issues} open issues!`);
+        console.log(`Found Milestone ${milestone.title}`);
+
+        if (milestone.open_issues > 0) {
+            core.setFailed(`Milestone ${milestone.title} still has ${milestone.open_issues} open issues!`);
         } else {
             console.log(`Milestone has no issues open.`)
         }
 
     }).catch((error) => {
         if (error.status === 404) {
-            core.setFailed(`Milestone with number ${milestoneNumber} Not Found!`);
+            core.setFailed(`Milestone ${milestoneTitle} Not Found!`);
         } else {
             console.debug(error);
             core.setFailed('Unknown Error!')
